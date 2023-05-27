a84735da615d8029ec881a28803bf5c0b19e5c2c
Aral Roca <aral-rg@hotmail.com>
2023-05-27 18:31:43 +0200

    Remove Lodash dependency: reducing package size (3kb to 300B)

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,49 @@
-import mergeWith from "lodash/mergeWith"
-
 export default function defaultComposer<T>(...args: Partial<T>[]): T {
-  return args.reduce(_defaultComposer<Partial<T>>, {} as T) as T
+  return args.reduce(compose, args[0]) as T;
 }
 
-function _defaultComposer<T>(defaults: T, originalObject: T): T {
-  return mergeWith(defaults, originalObject, (defaultsValue: T[keyof T], originalObjectValue) => {
-    if (isEmpty(originalObjectValue) && !isEmpty(defaultsValue)) {
-      return defaultsValue
+function compose(defaults, originalObject) {
+  const result = {};
+  const allKeys = new Set([
+    ...Object.keys(defaults),
+    ...Object.keys(originalObject),
+  ]);
+
+  for (let key of allKeys) {
+    const defaultsValue = defaults[key];
+    const originalObjectValue = originalObject[key];
+
+    if (isEmpty(originalObjectValue) && key in defaults) {
+      result[key] = defaultsValue;
+      continue;
     }
-    if (typeof originalObjectValue === 'object' && !Array.isArray(originalObjectValue) && defaultsValue) {
-      return _defaultComposer(defaultsValue, originalObjectValue)
+
+    if (
+      typeof originalObjectValue === "object" &&
+      originalObjectValue !== null &&
+      !Array.isArray(originalObjectValue) &&
+      defaultsValue
+    ) {
+      result[key] = compose(defaultsValue, originalObjectValue);
+      continue;
     }
-    return originalObjectValue
-  }) as T
+
+    result[key] = originalObjectValue;
+  }
+
+  return result;
 }
 
 function isEmptyObject<T>(object: T) {
-  if (typeof object !== 'object' || object === null) return false;
+  if (typeof object !== "object" || object === null) return false;
   return Object.keys(object).length === 0;
 }
 
 function isEmpty(value: any) {
-  return value === undefined || value === '' || value === null || isEmptyObject(value)
+  return (
+    value === undefined ||
+    value === "" ||
+    value === null ||
+    isEmptyObject(value)
+  );
 }
